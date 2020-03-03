7ac87d33e2f7093145765819b4c05e361723c664
Joao Vitor Maia <joaocampo2@hotmail.com>
2020-03-03 14:19:02 -0300

    Add docstring for actions

--- clitt/actions.py
+++ clitt/actions.py
@@ -1,31 +1,74 @@
-from tweepy import TweepError
+import tweepy
 
 from .interface import show_message, show_tweet, show_user
 
 
-def dm(api, target, content):
+def dm(api: tweepy.API, target: str, content: str):
+    """
+        Sends a direct message to target user
+
+        Keyword arguments:
+        api -- API instance for handling the request
+        target -- Target user's screename (e.g. @jake/jake)
+        content -- String that will be sent as message
+    """
+
     target = target.replace("@", "")
     user = api.get_user(target)
     api.send_direct_message(user.id, content)
 
 
-def search(api, query, count):
+def search(api: tweepy.API, query: str, count: int):
+    """
+        Searches for tweets containing the input string
+
+        Keyword arguments:
+        api -- API instance for handling the request
+        query -- String passed as search query for the API
+        count -- Maximum number of results the API will return
+    """
+
     results = api.search(query, count=count)
     for result in results:
         show_tweet(result)
 
 
-def user(api, query, count):
+def user(api: tweepy.API, query: str, count: int):
+    """
+        Searches for users related to the input string
+
+        Keyword arguments:
+        api -- API instance for handling the request
+        query -- String passed as search query for the API
+        count -- Maximum number of results the API will return
+    """
+
     results = api.search_users(query, count=count)
     for user in results:
         show_user(user)
 
 
-def post(api, content):
+def post(api: tweepy.API, content: str):
+    """
+        Update the status for currently logged user (basically, this methods tweets)
+
+        Keyword arguments:
+        api -- API instance for handling the request
+        content -- String that will be posted
+    """
+
     api.update_status(content)
 
 
-def chat(api, user):
+def chat(api: tweepy.API, user: str):
+    """
+        Search and displays private chat with target user
+
+        Keyword arguments:
+        api -- API instance for handling the request
+        user -- Target user's screename (e.g. @jake/jake)
+    """
+
     try:
         user = user.replace("@", "")
         user = api.get_user(user)
@@ -41,11 +84,19 @@ def chat(api, user):
                 and int(message.message_create["target"]["recipient_id"]) == user.id
             ):
                 show_message(message, me, reverse=True)
-    except TweepError:
+    except tweepy.TweepError:
         print("Sorry, user not found")
 
 
-def read(api, count):
+def read(api: tweepy.API, count: int):
+    """
+        Read currently logged user's timeline
+
+        Keyword arguments:
+        api -- API instance for handling the request
+        count -- Maximum number of results the API will return
+    """
+
     public_tweets = api.home_timeline(count=count)
     for tweet in public_tweets:
         show_tweet(tweet)
